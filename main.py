a9ad5437bb8e557082ebc34da8c357d193cf5ea4
Diego Tavares <diego.tavares@itaipuparquetec.org.br>
2025-03-25 15:23:57 -0300

    commit teste

--- main.py
+++ main.py
@@ -2,4 +2,16 @@ c = "o gabriel é meu parceiro"
 
 d = "Um dois tres"
 
-print(d, c)
+e = "num sei, num consigo"
+
+def funcao():
+    alpha =input("Digite uma letra de c a e")
+    if alpha == c:
+        print(d, c)
+    if alpha == d:
+        print(d)
+    else:
+        print(e)
+
+if __name__ == '__main__':
+     capacitacao = funcao()   
