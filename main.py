f2077a17d159328987b26575d1210b81e9f4f1b3
Diego Tavares <diego.tavares@itaipuparquetec.org.br>
2025-03-25 16:35:12 -0300

    corrigido o b

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ d = "Um dois tres"
 e = "num sei, num consigo"
 
 def funcao():
-    alpha =input("Digite uma letra de c a e: ")
+    alpha =input("Digite uma letra de b a e: ")
     if alpha == 'b':
         print(b)
-    if alpha == "c":
-        print(d, c)
+
+    if alpha == 'c':
+        print(c)
+
     if alpha == 'd':
         print(d)
+
     else:
         print(e)
 
